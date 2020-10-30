480e985623c3c09ec016eb9da652acefe693c83c
Ravi <ravikgupta32@gmail.com>
2020-10-30 19:52:48 +0530

    first

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,17 @@
+export const MenuItems = [
+  {
+    title: "LOGIN",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "ABOUT",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "CONTACT US",
+    url: "#",
+    cName: "nav-links",
+  },
+];
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCpNN8yJlBFAj-QcrRRqAmfMMzWsnqSCRU",
+  authDomain: "hospital-managment-8a4bd.firebaseapp.com",
+  databaseURL: "https://hospital-managment-8a4bd.firebaseio.com",
+  projectId: "hospital-managment-8a4bd",
+  storageBucket: "hospital-managment-8a4bd.appspot.com",
+  messagingSenderId: "387124254367",
+  appId: "1:387124254367:web:089f13726a8dec072f34aa",
+  measurementId: "G-HVYPJ3VFGN",
+};
